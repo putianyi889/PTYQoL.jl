1a7c63a542e76be8c58d7c28a40bf57f90fc56c3
Tianyi Pu <912396513@qq.com>
2023-11-30 08:11:57 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,13 @@ using PTYQoL
 makedocs(
     sitename = "PTYQoL",
     format = Documenter.HTML(),
+    doctest = false,
     modules = [PTYQoL]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/putianyi889/PTYQoL.jl.git"
+)
