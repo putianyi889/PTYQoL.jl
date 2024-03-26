6cd60e0fbda9de0058e236f749a1d173d25528cb
Tianyi Pu <912396513@qq.com>
2024-03-26 11:49:14 +0000

    remove breaking things

--- ext/PTYQoLAlgebraicNumbersExt.jl
+++ ext/PTYQoLAlgebraicNumbersExt.jl
@@ -1,25 +1,13 @@
 module PTYQoLAlgebraicNumbersExt
 
 import AlgebraicNumbers: sin_alg, cos_alg, AlgebraicNumber
-import Base: sinpi, cospi, tanpi, sincospi, sind, cosd, sincosd, promote_rule, float
+import Base: promote_rule, float
 
-export tan_alg, sincos_alg
-
-tan_alg(x) = sin_alg(x) // cos_alg(x)
-sincos_alg(x) = (sin_alg(x), cos_alg(x))
-
-for (fd, fpi, falg) in ((:sind, :sinpi, :sin_alg), (:cosd, :cospi, :cos_alg), (:tand, :tanpi, :tan_alg), (:sincosd, :sincospi, :sincos_alg))
-    @eval begin
-        $fpi(x::Rational) = $falg(x)
-        $fpi(x::AlgebraicNumber) = $falg(x)
-        $fd(x::Integer) = $falg(x//180)
-        $fd(x::Rational) = $falg(x//180)
-    end
-end
-
+# https://github.com/anj1/AlgebraicNumbers.jl/pull/35
 promote_rule(::Type{<:AlgebraicNumber}, ::Type{T}) where T <: AbstractFloat = T
 float(x::AlgebraicNumber) = x.apprx
 (::Type{T})(x::AlgebraicNumber) where T<:AbstractFloat = T(x.apprx)
+
 AlgebraicNumber(x::AlgebraicNumber) = x
 
 end # module
